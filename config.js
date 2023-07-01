02d626cad8a3474c1ac20fb258559de336b9a3c5
Nethmirabota <119153440+Nethmirabota@users.noreply.github.com>
2023-07-01 19:56:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94711670990', 'Nethmira', true],
+  ['94711670990', 'Nethmira', true], 
   [''] 
 ] //Number of owners
 
